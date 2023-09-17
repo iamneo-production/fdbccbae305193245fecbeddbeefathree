87c93284901f34951350124f143d0d8661340e51
Iamneo <techadmin@iamneo.ai>
2023-09-17 09:45:25 +0000

    Backup Commit

--- KathakBookingSystem/Program.cs
+++ KathakBookingSystem/Program.cs
@@ -6,7 +6,9 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-builder.Services.AddDbContext<ApplicationDbContext>(options=>options.UseSqlServer(options.Configuration.GetConnectionString("DevConnection")));
+builder.Services.AddDbContext<ApplicationDbContext>
+(options=>options.UseSqlServer(builder.Configuration.
+GetConnectionString("DevConnection")));
 
 var app = builder.Build();
 
