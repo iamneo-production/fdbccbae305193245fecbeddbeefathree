ab9aa55b850ea15474301292bd7221312c4d78a5
Iamneo <techadmin@iamneo.ai>
2023-09-17 05:48:36 +0000

    Backup Commit

--- KathakBookingSystem/Models/Class.cs
+++ KathakBookingSystem/Models/Class.cs
@@ -9,6 +9,7 @@ namespace KathakBookingSystem.Models
         public date StartTime{get;set;}
         public date EndTime{get;set;}
         public int Capacity{get;set;}
-        
+        public ICollection<Student> Student{get set;} 
+
     }
 }
--- KathakBookingSystem/Models/Student.cs
+++ KathakBookingSystem/Models/Student.cs
@@ -4,4 +4,10 @@ using System.Collections.Generic;
 namespace KathakBookingSystem.Models
 {
     // Write your Student class here...
+    public class Student{
+        public int StudentID{get;set;}
+        public string Name {get;set;}
+        public string Email{get;set;}
+        public int ClassID {get;set;}
+    }
 }
