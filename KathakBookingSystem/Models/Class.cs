3785bf1c91aed764bfdc420150d7690da2b458ea
Iamneo <techadmin@iamneo.ai>
2023-09-17 05:43:36 +0000

    Backup Commit

--- KathakBookingSystem/Models/Class.cs
+++ KathakBookingSystem/Models/Class.cs
@@ -4,4 +4,11 @@ using System.Collections.Generic;
 namespace KathakBookingSystem.Models
 {
     // Write your Class table class here...
+    public class Class{
+        public int ClassID {get;set;}
+        public date StartTime{get;set;}
+        public date EndTime{get;set;}
+        public int Capacity{get;set;}
+        
+    }
 }
