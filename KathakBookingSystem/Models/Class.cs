1aee2ed927c67c1d6c6b9ecc817fb3afad50f066
Iamneo <techadmin@iamneo.ai>
2023-09-17 05:53:37 +0000

    Backup Commit

--- KathakBookingSystem/Models/Class.cs
+++ KathakBookingSystem/Models/Class.cs
@@ -6,10 +6,10 @@ namespace KathakBookingSystem.Models
     // Write your Class table class here...
     public class Class{
         public int ClassID {get;set;}
-        public date StartTime{get;set;}
-        public date EndTime{get;set;}
+        public DateTime StartTime{get;set;}
+        public DateTime EndTime{get;set;}
         public int Capacity{get;set;}
-        public ICollection<Student> Student{get set;} 
+        public ICollection<Student> Student{get; set;} 
 
     }
 }
--- KathakBookingSystem/Models/KathakClassBookingException.cs
+++ KathakBookingSystem/Models/KathakClassBookingException.cs
@@ -3,4 +3,11 @@
 namespace KathakBookingSystem.Models
 {
     // Write your KathakClassBookingException here...
+    public class KathakClassBookingException:Exception
+    {
+        public KathakClassBookingException():base()
+        {
+
+        }
+    }
 }
