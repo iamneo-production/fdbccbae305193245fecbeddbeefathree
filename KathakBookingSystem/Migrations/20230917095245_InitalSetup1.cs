bb9e7b5b37696502260b79e7c5e9dd1731639a79
Iamneo <techadmin@iamneo.ai>
2023-09-17 09:56:02 +0000

    Backup Commit

---
+++ KathakBookingSystem/Migrations/20230917095245_InitalSetup1.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace KathakBookingSystem.Migrations
+{
+    public partial class InitalSetup1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
